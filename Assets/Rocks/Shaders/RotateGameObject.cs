89f976ae15a138f653987fc25e373a1917fddcc5
bababui328 <markevitchmir@yandex.ru>
2026-04-02 18:20:23 +1000

    Смог сделать блид своей SumulationBaseScene

--- Assets/Rocks/Shaders/RotateGameObject.cs
+++ Assets/Rocks/Shaders/RotateGameObject.cs
@@ -15,7 +15,7 @@ public class RotateGameObject : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 		if (local) {
-			transform.RotateAroundLocal(transform.up, Time.fixedDeltaTime*rot_speed_x);
+			transform.Rotate(new Vector3(0f, Time.fixedDeltaTime*rot_speed_x, 0f), Space.Self);
 		} else {
 			transform.Rotate(Time.fixedDeltaTime*new Vector3(rot_speed_x,rot_speed_y,rot_speed_z), Space.World);
 		}
